3ed0d9daa58ce8d7cc99cae6c224f933897340b1
MannySotoRuiz <72245427+MannySotoRuiz@users.noreply.github.com>
2023-02-14 23:10:48 -0500

    added about in backed and fecth from frontend

--- back-end/app.js
+++ back-end/app.js
@@ -78,5 +78,26 @@ app.post('/messages/save', async (req, res) => {
   }
 })
 
+app.get('/about', async (req, res) => {
+  const paragraph1 = `I am a senior studying Computer Science. I was originally a biology major and on the pre-med
+  track but I decided to change my studies since I wasn't enjoying it anymore. I am glad that I made the switch since I have been enjoying CS since my first day of coding.`;
+
+  const paragraph2 = `In my free time, I like to stay active. Soccer and boxing are two of my favorite sports. Also, I like to spend my time listening to music. I was originally born in Mexico City 
+  but moved to New York when I was 2 years old. Just last year, I was able to travel back to Mexico for the first time since I moved to the United States.`;
+
+  const paragraph3 = `After graduation, my goal is to work as a software engineer and continue to develop my skills and knowledge in the tech field.
+  I am excited for what the future holds and I can't wait to see what opportunities come my way.`;
+
+  const picture = "https://soto-ruiz-art.s3.amazonaws.com/foo_pic.PNG";
+
+  return res.json({
+    name: "Manny Soto Ruiz",
+    paragraphs: [paragraph1, paragraph2, paragraph3],
+    picture: picture,
+    status: "all good",
+  })
+
+})
+
 // export the express app we created to make it available to other modules
 module.exports = app // CommonJS export style!
--- front-end/src/About.js
+++ front-end/src/About.js
@@ -1,30 +1,44 @@
-import React from 'react'
-import placeHolder from "./placeholder.png";
+import React, { useEffect, useState } from 'react';
 
 const About = () => {
+
+    const [name, setName] = useState("");
+    const [paragraphs, setParagraphs] = useState(null);
+    const [image, setImage] = useState("");
+
+    useEffect(() => {
+        const fetchAboutData = async () => {
+            const response = await fetch("http://localhost:5002/about");
+            const json = await response.json();
+
+            if (response.status === 200) {
+                setName(json.name);
+                setParagraphs(json.paragraphs);
+                setImage(json.picture);
+            }
+        }
+
+        fetchAboutData();
+    }, [])
+
+
     return (
         <div>
-            <h1>Manny Soto Ruiz</h1>
+            <h1>{name}</h1>
     
             <div 
             style={{ display: "flex", justifyContent: "center", alignItems: "center", padding: "30px", flexDirection: "column", gap: "3rem"}}
             >   
                 {/* paragraphs */}
-                <p style={{ paddingRight: "100px", paddingLeft: "100px" }}>I am a senior studying Computer Science. I was originally a biology major and on the pre-med
-                    track but I decided to change my studies since I wasn't enjoying it anymore. I am glad that I made the switch since I have been enjoying CS since my first day of coding.
-                </p>
-                <p style={{ paddingRight: "100px", paddingLeft: "100px" }}>
-                    In my free time, I like to stay active. Soccer and boxing are two of my favorite sports. Also, I like to spend my time listening to music. I was originally born in Mexico City 
-                    but moved to New York when I was 2 years old. Just last year, I was able to travel back to Mexico for the first time since I moved to the United States.
-                </p>
-                <p style={{ paddingRight: "100px", paddingLeft: "100px" }}>
-                    After graduation, my goal is to work as a software engineer and continue to develop my skills and knowledge in the tech field.
-                    I am excited for what the future holds and I can't wait to see what opportunities come my way.
-                </p>
+                {paragraphs?.map((p, idx) => {
+                    return (
+                        <p style={{ paddingRight: "100px", paddingLeft: "100px" }} key={idx}>{p}</p>
+                    );
+                })}
     
                 {/* image */}
                 <div style={{ width: "200px", height: "200px" }}>
-                    <img style={{ objectFit: "cover", height: "100%", width: "100%", borderRadius: "20px" }} src={placeHolder} alt=""/>
+                    <img style={{ objectFit: "cover", height: "100%", width: "100%", borderRadius: "20px" }} src={image} alt=""/>
                 </div>
             </div>
         </div>
